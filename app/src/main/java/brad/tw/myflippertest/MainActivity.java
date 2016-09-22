ff5820b8752f886535f855010f29a18e2d822c20
Brad Chao <brad@brad.tw>
2016-09-22 12:12:43 +0800

    v2

--- app/src/main/java/brad/tw/myflippertest/MainActivity.java
+++ app/src/main/java/brad/tw/myflippertest/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     private ViewFlipper vf;
+    private Animation animInLeftRight, animInRightLeft, animOutLeftRight, animOutRightLeft;
 
 
     @Override
@@ -15,11 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        animInLeftRight = AnimationUtils.loadAnimation(this, R.anim.in_leftright);
+        animOutLeftRight = AnimationUtils.loadAnimation(this, R.anim.out_leftright);
+
+
         vf = (ViewFlipper)findViewById(R.id.activity_main);
 
         vf.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                vf.setInAnimation(animInLeftRight);
+                vf.setOutAnimation(animOutLeftRight);
                 vf.showNext();
                 return false;
             }
